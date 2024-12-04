3b12ec607ef3de3d48532a97fd8995e472499ad1
DragonFang <dragonfang576@gmail.com>
2024-12-04 11:35:01 -0600

    day 5 initialize

---
+++ day 5/main.py
@@ -0,0 +1,10 @@
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+if __name__ == '__main__':
+    src = 'example.txt'
+    # src = 'data.txt'
+    data = open(src, 'r').read().split('\n')
